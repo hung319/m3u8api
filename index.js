04de1511714e67500a7a381c43b7df74fbec056e
Hoàng Phi Hùng <83570379+hung319@users.noreply.github.com>
2025-05-01 21:59:16 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 const animetRoutes = require('./routes/animet');
+const anime47Routes = require('./routes/anime47');
 
 const PORT = 3000;
 
 // Gắn routes
 app.use('/animet', animetRoutes);
+app.use('/anime47', anime47Routes);
 
 app.listen(PORT, () => {
     console.log(`Mahiro server đã sẵn sàng phục vụ Yuu Onii-chan ở http://localhost:${PORT} rồi nè UwU`);
