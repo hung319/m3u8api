32a620789af051bee323058203812d209166d5cc
Hoàng Phi Hùng <83570379+hung319@users.noreply.github.com>
2025-05-02 12:02:18 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const express = require('express');
 const app = express();
 const animetRoutes = require('./routes/animet');
 const anime47Routes = require('./routes/anime47');
+const animevietsubRoutes = require('./routes/animevietsub');
 
 const PORT = 3000;
 
 // Gắn routes
 app.use('/animet', animetRoutes);
 app.use('/anime47', anime47Routes);
+app.use('/animevietsub', animevietsubRoutes);
+
 
 const hostname = '0.0.0.0';
 app.listen(PORT, () => {
